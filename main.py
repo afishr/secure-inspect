8175c9ea55d183641f64d4afb4429ddfc26f5d98
Alexandru Calugari <afishr00@gmail.com>
2020-09-07 22:33:16 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from tkinter import *
+from tkinter import filedialog
+from view_audit_structure import compute_audit_structure, output_structure
+
+class Window(Frame):
+
+	def __init__(self, master=None):
+		Frame.__init__(self, master)        
+		self.master = master
+		self.pack(fill=BOTH, expand=1)
+
+		openButton = Button(self, text="Open file", command=self.openProcessAndOutputFile)
+		openButton.grid(column=1)
+
+		saveButton = Button(self, text="Save file", command=self.saveFile)
+		saveButton.grid(column=1)
+
+		self.output = Text()
+		self.output.pack(fill=BOTH, expand=1)
+
+	def saveFile(self):
+		file = filedialog.asksaveasfile(mode="w")
+		f = open(file.name, "w")
+		f.write(self.output.get("1.0",END))
+		f.close()
+
+	def openProcessAndOutputFile(self):
+		file = filedialog.askopenfile(mode="r", filetypes = (("Audit files", "*.audit") ,("All files", "*.*")))
+
+		if not file:
+			return
+		
+		f = open(file.name, "r")
+
+		structure = compute_audit_structure(f.read())
+
+		form = '{}{}'
+
+		self.output.config(state=NORMAL)
+
+		for (_, depth, text) in structure:
+				self.output.insert(END, form.format('  '*depth, text))
+				self.output.insert(END, '\n')
+
+		self.output.config(state=DISABLED)
+			
+root = Tk()
+app = Window(root)
+root.wm_title("Security Inspector")
+root.geometry("500x500")
+root.mainloop()
